59183e4355534cd34f996bc3c475b0248aaff3bf
John Breiner <jbreiner@gmail.com>
2016-12-14 03:43:09 -0600

    Added first model

--- App/Controllers/Posts.php
+++ App/Controllers/Posts.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use \Core\View;
+use App\Models\Post;
 
 /**
  * Posts controller
@@ -19,10 +20,13 @@ class Posts extends \Core\Controller
      */
     public function indexAction()
     {
+        $posts = Post::getAll();
         //echo 'Hello from the index action in the Posts controller!';
         //echo '<p>Query string parameters: <pre>' .
         //     htmlspecialchars(print_r($_GET, true)) . '</pre></p>';
-        View::renderTemplate('Posts/index.html');
+        View::renderTemplate('Posts/index.html', [
+            'posts' => $posts
+            ]);
     }
 
     /**
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,6 +6,28 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit8f927e6945b11999cd384b4bfbc6d038
 {
+    public static $prefixLengthsPsr4 = array (
+        'C' => 
+        array (
+            'Core\\' => 5,
+        ),
+        'A' => 
+        array (
+            'App\\' => 4,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Core\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/Core',
+        ),
+        'App\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/App',
+        ),
+    );
+
     public static $prefixesPsr0 = array (
         'T' => 
         array (
@@ -30,6 +52,8 @@ class ComposerStaticInit8f927e6945b11999cd384b4bfbc6d038
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit8f927e6945b11999cd384b4bfbc6d038::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit8f927e6945b11999cd384b4bfbc6d038::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit8f927e6945b11999cd384b4bfbc6d038::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit8f927e6945b11999cd384b4bfbc6d038::$classMap;
 
